e8d46f05fc7f23b93ebeaddb69355da5a36f92ea
Cesar Terrazas <cesar.terrazas.g@gmail.com>
2016-02-06 13:57:12 -0700

    Formatting changes

--- SRM 681 DIV 2/src/CoinFlipsDiv2.java
+++ SRM 681 DIV 2/src/CoinFlipsDiv2.java
@@ -1,15 +1,16 @@
 public class CoinFlipsDiv2 {
-	public int countCoins(String state) {
+    public int countCoins(String state) {
         int count = 0;
         for (int i = 0; i < state.length(); i++) {
-            if(i > 0 && !equalsToPreviousCoin(state, i)) {
+            if (i > 0 && !equalsToPreviousCoin(state, i)) {
                 count++;
-            } else if(i < state.length() - 1 && !equalsToNextCoin(state, i)) {
+            } else if (i < state.length() - 1 && !equalsToNextCoin(state, i)) {
                 count++;
             }
         }
         return count;
     }
+
     private boolean equalsToPreviousCoin(String state, int i) {
         return state.charAt(i) == state.charAt(i - 1);
     }
