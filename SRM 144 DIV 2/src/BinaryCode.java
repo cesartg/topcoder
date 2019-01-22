138aa60d47d556aa45e0b3528026ee44186fc66d
César Terrazas <cesar.terrazas.g@gmail.com>
2019-01-21 19:55:25 -0700

    Simplify solution

--- SRM 144 DIV 2/src/BinaryCode.java
+++ SRM 144 DIV 2/src/BinaryCode.java
@@ -1,47 +1,51 @@
 public class BinaryCode {
     public String[] decode(String message) {
         String[] decodedMessages = new String[2];
-        decodedMessages[0] = decode(message, 0);
-        decodedMessages[1] = decode(message, 1);
+        decodedMessages[0] = decode(message, "0");
+        decodedMessages[1] = decode(message, "1");
         return decodedMessages;
     }
 
-    private String decode(final String encodedMessage, final int assumedFirstCharacter) {
-        final int messageLength = encodedMessage.length();
+    private String decode(String encodedMessage, String decodedMessageAssumedFirstCharacter) {
+        int messageLength = encodedMessage.length();
         if (messageLength == 1) {
-            if (encodedMessage.equals(assumedFirstCharacter + "")) {
-                return assumedFirstCharacter + "";
+            if (encodedMessage.equals(decodedMessageAssumedFirstCharacter)) {
+                return decodedMessageAssumedFirstCharacter;
             }
             return "NONE";
         }
-        final int[] encodedMessageInts = stringToIntArray(encodedMessage);
-        final int[] decodedMessageInts = new int[messageLength];
-        decodedMessageInts[0] = assumedFirstCharacter;
-        decodedMessageInts[1] = encodedMessageInts[0] - assumedFirstCharacter;
-        if (decodedMessageInts[1] != 0 && decodedMessageInts[1] != 1) {
+        final int[] encodedMessageDigits = stringToIntArray(encodedMessage);
+        final int[] decodedMessageDigits = new int[messageLength];
+        int decodedMessageAssumedFirstDigit = Integer.parseInt(decodedMessageAssumedFirstCharacter);
+        decodedMessageDigits[0] = decodedMessageAssumedFirstDigit;
+        decodedMessageDigits[1] = encodedMessageDigits[0] - decodedMessageAssumedFirstDigit;
+        if (isInvalidBinaryDigit(decodedMessageDigits[1])) {
             return "NONE";
         }
         for (int i = 2; i < messageLength; i++) {
-            decodedMessageInts[i] = encodedMessageInts[i - 1] - decodedMessageInts[i - 1] -
-                    decodedMessageInts[i - 2];
-            if (decodedMessageInts[i] != 0 && decodedMessageInts[i] != 1) {
+            decodedMessageDigits[i] = encodedMessageDigits[i - 1] - decodedMessageDigits[i - 1] -
+                    decodedMessageDigits[i - 2];
+            if (isInvalidBinaryDigit(decodedMessageDigits[i])) {
                 return "NONE";
             }
         }
-        if (encodedMessageInts[messageLength - 1] != decodedMessageInts[messageLength - 2] +
-                decodedMessageInts[messageLength - 1]) {
+        if (encodedMessageDigits[messageLength - 1] != decodedMessageDigits[messageLength - 2] +
+                decodedMessageDigits[messageLength - 1]) {
             return "NONE";
         }
-        return intArrayToString(decodedMessageInts);
+        return intArrayToString(decodedMessageDigits);
     }
 
     private int[] stringToIntArray(String str) {
-        final int stringLength = str.length();
-        final int[] intArray = new int[stringLength];
-        for (int i = 0; i < stringLength; i++) {
-            intArray[i] = Integer.parseInt(str.substring(i, i + 1));
+        int[] intArray = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            intArray[i] = Character.getNumericValue(str.charAt(i));
         }
         return intArray;
+    }
+
+    private boolean isInvalidBinaryDigit(int digit) {
+        return digit != 0 && digit != 1;
     }
 
     private String intArrayToString(int[] intArray) {
