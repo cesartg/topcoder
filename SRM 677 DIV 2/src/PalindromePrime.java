7f6784a4e33b8468cd14fbc4d924a108e7020eec
César Terrazas <cesar.terrazas.g@gmail.com>
2019-01-21 20:03:09 -0700

    Simplify solution

--- SRM 677 DIV 2/src/PalindromePrime.java
+++ SRM 677 DIV 2/src/PalindromePrime.java
@@ -2,37 +2,32 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PalindromePrime {
-    private static List<Integer> palindromePrimesList = computePalindromePrimeList();
+    private static boolean[] isPalindromePrimeArray = createIsPalindromePrimeArray();
 
     public int count(int l, int r) {
         int count = 0;
-        for (Integer palindromePrime : palindromePrimesList) {
-            if (palindromePrime < l) {
-                continue;
-            }
-            if (palindromePrime >= l && palindromePrime <= r) {
+        for (int i = l; i <= r; i++) {
+            if (isPalindromePrimeArray[i]) {
                 count++;
-            } else {
-                return count;
             }
         }
         return count;
     }
 
-    private static List<Integer> computePalindromePrimeList() {
-        List<Integer> palindromePrimesList = new LinkedList<>();
-        for(int i = 2; i <= 1000; i++) {
-            if(isPalindrome(i) && isPrime(i)) {
-                palindromePrimesList.add(i);
+    private static boolean[] createIsPalindromePrimeArray() {
+        boolean[] isPalindromePrimeArray = new boolean[1001];
+        for (int i = 2; i <= 1000; i++) {
+            if (isPalindrome(i) && isPrime(i)) {
+                isPalindromePrimeArray[i] = true;
             }
         }
-        return palindromePrimesList;
+        return isPalindromePrimeArray;
     }
 
     private static boolean isPalindrome(int i) {
         String s = i + "";
         for (int j = 0; j < s.length() / 2; j++) {
-            if(s.charAt(j) != s.charAt(s.length() - j - 1)) {
+            if (s.charAt(j) != s.charAt(s.length() - j - 1)) {
                 return false;
             }
         }
@@ -40,8 +35,8 @@ public class PalindromePrime {
     }
 
     private static boolean isPrime(int i) {
-        for (int j = (int)Math.sqrt(i); j >= 2; j--) {
-            if(i % j == 0) {
+        for (int j = (int) Math.sqrt(i); j >= 2; j--) {
+            if (i % j == 0) {
                 return false;
             }
         }
